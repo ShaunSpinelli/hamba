ee235d778480db4eed5d703207bdb3fb6d7cf060
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-01 15:58:36 +1000

    skid authentication on index page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,7 @@
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index]
 
   # GET /jobs
   # GET /jobs.json
