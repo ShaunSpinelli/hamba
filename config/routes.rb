ee909e29ace22d14834123abffc85c1c2f2f0bf4
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-02 00:20:22 +1000

    added custom post method for makemove

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
-
+  #sget '/charges/new/:id', to: 'charges#new', as: 'new_charges_path'
+  post '/jobs/makemove/:id', to: 'jobs#makemove', as: 'makemove_jobs'
   resources :jobs
   devise_for :users
   resources :users
