ecfe18010f661077785f8c045025237e0e567f68
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-01 15:29:20 +1000

    added nesting for drop off

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,8 @@
 class Job < ApplicationRecord
   belongs_to :user
   has_one :pick_up
+  has_one :drop_off
   accepts_nested_attributes_for :pick_up
+  accepts_nested_attributes_for :drop_off
+  
 end
