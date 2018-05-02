9c913fda10a5104440b0f10747dd41c046a44b82
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-02 15:24:09 +1000

    took logic out of page and added it to helper

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,17 +1,33 @@
 module JobsHelper
 
+    def job_options(job)
+        if job.user == current_user
+            job_applications(job)
+        else
+            apply_job(job)
+        end
+    end
+    
+
     def apply_job(job)
         if Move.where(user_id: current_user.id , job_id: job.id).exists?
-            content_tag(:p, "You have applied for this job")
+            return content_tag(:p, "You have applied for this job")
         else
-            link_to 'Accept Job', makemove_jobs_path(@job), method: :postcode
+            return link_to 'Accept Job', makemove_jobs_path(@job), method: :post
         end
     end
     
     def job_applications(job)
-        if job.user == current_user
-            Move.where(job_id: job.id)
+        if Move.find_by(job_id: job.id) != nil
+            content_tag(:div, class: "") do
+                Move.where(job_id: job.id).each do |move|
+                    concat link_to move.user.email, user_path(move.user)
+                end
+            end
+        else
+            return content_tag(:p, "no want to move your stuff")
         end
     end
 
 end
+
