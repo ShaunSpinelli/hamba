32bd8b6a26e5bad478b96c8bce2640d0d69ecf67
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-02 13:31:44 +1000

    created apply job method

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,11 @@
 module JobsHelper
+
+    def apply_job(job)
+        if Move.where(user_id: current_user.id , job_id: job.id).exists?
+            content_tag(:p, "You have applied for this job")
+        else
+            link_to 'Accept Job', makemove_jobs_path(@job), method: :postcode
+        end
+    end
+    
 end
