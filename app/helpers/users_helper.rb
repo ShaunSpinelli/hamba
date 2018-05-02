0b0b6b9b7583f212a4af38ce43f7da95cb174388
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-02 19:42:40 +1000

    added helpers for users view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,27 @@
 module UsersHelper
+
+    def visiter_check(user) # helper method run in view to check who the vister is
+        if user == current_user
+            owner(user)
+        else
+            guest(user)
+        end
+    end
+    
+    def guest(user)# views for a guest
+        # find move were job.user == the current user and :user = user
+        moves = Move.where(user_id: user.id)
+        moves.each do |move|
+            if move.job.user = current_user
+                return link_to 'Confirm Job', confirmmove_jobs_path(move.id), method: :post
+            end
+            return content_tag(:p, "no want to move your stuff")
+        end
+    end
+
+    def owner(user)#views for profile owner
+        link_to 'Edit', edit_user_path(user)
+    end
+    
+
 end
