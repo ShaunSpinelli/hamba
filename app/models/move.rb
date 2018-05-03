68b643095710a96b041483e907c4a875114b54f7
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-04 00:11:24 +1000

    added send notfication after after move is created

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,4 +1,16 @@
 class Move < ApplicationRecord
   belongs_to :user
   belongs_to :job
+
+  after_create :send_notification
+
+  def send_notification
+    UserMailer.send_job_notification_email(self).deliver
+  end
+  
+
+
+
+
+
 end
