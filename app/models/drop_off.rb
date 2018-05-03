3901e1e443a185508f3500d0c4ab960d05d1a9e6
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-03 10:36:45 +1000

    added geocoder and full adress method

--- app/models/drop_off.rb
+++ app/models/drop_off.rb
@@ -1,3 +1,9 @@
 class DropOff < ApplicationRecord
     belongs_to :job
+    geocoded_by :full_address   # can also be an IP address
+    after_validation :geocode          # auto-fetch coordinates
+
+    def full_address
+        "#{address} #{city} #{state} #{postcode}"
+    end
 end
