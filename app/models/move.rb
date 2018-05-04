0f8d27e22ee09f9e2f7a9e832ff4ddbdc6fe5d02
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-04 15:37:28 +1000

    added accpatence method to send confirmation email

--- app/models/move.rb
+++ app/models/move.rb
@@ -3,9 +3,13 @@ class Move < ApplicationRecord
   belongs_to :job
 
   after_create :send_notification
-
+  after_update :send_acceptance
   def send_notification
     UserMailer.send_job_notification_email(self).deliver
+  end
+
+  def send_acceptance
+    UserMailer.send_job_confirmation_email(self).deliver
   end
   
 
