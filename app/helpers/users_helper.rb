43831a1220763bbecabb6a936f4fc5e177367842
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-04 15:36:36 +1000

    added coments for a bug i need to fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,18 +8,17 @@ module UsersHelper
         end
     end
     
-    def guest(user)# views for a guest
-        # find move were job.user == the current user and :user = user
-        moves = Move.where(user_id: user.id)
+    def guest(user)
+        moves = Move.where(user_id: user.id) 
         moves.each do |move|
-            if move.job.user = current_user
+            if move.job.user = current_user # just finding the first one if current users if mover and job poster have more the one move with eachother
                 return link_to 'Confirm Job', confirmmove_jobs_path(move.id), method: :post
             end
             return content_tag(:p, "no want to move your stuff")
         end
     end
 
-    def owner(user)#views for profile owner
+    def owner(user)
         link_to 'Edit', edit_user_path(user)
     end
     
