a4da8cc12ba86ff8a99ef50a372dff994937da61
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-05 12:23:10 +1000

    added a confirm or decline options on the jobs page

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -13,7 +13,7 @@ module JobsHelper
         if Move.where(user_id: current_user.id , job_id: job.id).exists?
             return content_tag(:p, "You have applied for this job")
         else
-            return link_to 'Accept Job', makemove_jobs_path(@job), method: :post
+            return link_to 'Apply For Job', makemove_jobs_path(@job), method: :post
         end
     end
     
@@ -22,7 +22,9 @@ module JobsHelper
             content_tag(:div, class: "") do
                 concat content_tag(:p,"Users interested")
                 Move.where(job_id: job.id).each do |move|
-                    concat link_to move.user.email, user_path(move.user) 
+                    concat content_tag(:p, "#{move.user.full_name}")                    
+                    concat link_to 'Confirm Job', confirmmove_jobs_path({:id => move.id, :confirm => 'true'}), method: :post
+                    concat link_to 'Decline job', confirmmove_jobs_path({:id => move.id, :confirm => 'false'}), method: :post                  
                     concat content_tag(:p, "")
                 end
             end
