973478124f3a1127fedef827cc725a97b20bd827
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-07 16:14:27 +1000

    styling for nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,10 @@ module ApplicationHelper
     def admin_menu
         if current_user
             if current_user.has_role? :admin            
-                content_tag(:div, class: "") do
-                    concat content_tag(:p, "Admin tools:")
-                    concat link_to "Access Users ", users_path 
-                    concat link_to "Access Jobs", jobs_path
+                content_tag(:div, class: "ui secondary menu") do
+                    concat content_tag(:div, 'Admin tools', :class=>'ui right pointing  label' )
+                concat link_to "Access Users ", users_path, class: 'item'
+                    concat link_to "Access Jobs", jobs_path, class: 'item'
                 end
             end
         end  
