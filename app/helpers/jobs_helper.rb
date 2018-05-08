2251fe1929ba99204642b69a5e354c0d2912d842
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-08 15:39:26 +1000

    added helper for distance and tag colour

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -13,7 +13,7 @@ module JobsHelper
         if Move.where(user_id: current_user.id , job_id: job.id).exists?
             return content_tag(:p, "You have applied for this job")
         else
-            return link_to 'Apply For Job', makemove_jobs_path(@job), method: :post, class: 'ui positive button'
+            return link_to 'Apply For Job', makemove_jobs_path(@job), method: :post, class: 'ui posi    tive button'
         end
     end
     
@@ -24,6 +24,16 @@ module JobsHelper
         else
             return nil
         end
+    end
+
+    def tag_colour(job)
+        job.urgency == "Very" ? col = "red" : col = "teal"
+        return content_tag(:a,"#{job.urgency}", class:"ui #{col} ribbon label")
+    end
+
+    def distance(job)
+        distance = Geocoder::Calculations.distance_between([job.pick_up.latitude,job.pick_up.longitude], [job.drop_off.latitude,job.drop_off.longitude])
+        '%.1f' % distance * 1.6
     end
 
 end
