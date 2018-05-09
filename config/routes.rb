f478a3a5143f68e0a8bb9716d78579ace03b5df1
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-05-09 12:49:17 +1000

    added route for premium psot request

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get 'home/index'
   post '/jobs/makemove/:id', to: 'jobs#makemove', as: 'makemove_jobs'
   post '/jobs/confirmmove/:id', to: 'jobs#confirmmove', as: 'confirmmove_jobs'  
+  post '/jobs/premium/:id', to: 'jobs#premium', as: 'premium_jobs'  
   resources :jobs
   devise_for :users
   resources :users
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
